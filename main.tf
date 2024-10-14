1471eb912126034cd5b9861f60925c28cd21257e
Jiří Oláh <jiri.olah@cookielab.io>
2024-10-14 14:24:31 +0200

    feat(multiple domains): Add possibility to ACM for multiple zones and create DNS record in extra zones

--- main.tf
+++ main.tf
@@ -17,15 +17,17 @@ module "certificate" {
   }
 
   source  = "terraform-aws-modules/acm/aws"
-  version = "5.0.0"
+  version = "5.1.1"
 
   domain_name = local.main_domain
   zone_id     = var.domain_zone_id
 
-  subject_alternative_names = local.alternative_domains
+  subject_alternative_names = merge(local.alternative_domains, keys(extra_domains))
 
   validation_method   = "DNS"
   wait_for_validation = true
+
+  zones = var.extra_domains
 
   tags = local.tags
 }
@@ -338,8 +340,22 @@ resource "aws_cloudfront_distribution" "this" {
 resource "aws_route53_record" "this" {
   for_each = toset(var.domains)
 
-  zone_id = var.domain_zone_id
-  name    = each.value
+  zone_id = each.value
+  name    = each.key
+  type    = "A"
+
+  alias {
+    name                   = aws_cloudfront_distribution.this.domain_name
+    zone_id                = aws_cloudfront_distribution.this.hosted_zone_id
+    evaluate_target_health = false
+  }
+}
+
+resource "aws_route53_record" "extra" {
+  for_each = var.extra_domains
+
+  zone_id = each.value
+  name    = each.key
   type    = "A"
 
   alias {
