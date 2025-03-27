6486319d5f4f9088213478cfde94e4f6bc4d97d5
Jindrich Skupa <jindrich.skupa@gmail.com>
2025-03-27 14:45:29 +0100

    Fix empty cors and custom headers 2 (#38)
    * feat(cf): allow to set empty custom headers & security headers
    
    * feat(cf): allow to set empty custom headers & security headers
    
    * feat(cf): allow to set empty custom headers & security headers
    
    * feat(cf): allow to set empty custom headers & security headers

--- main.tf
+++ main.tf
@@ -1,8 +1,9 @@
 locals {
-  main_domain           = one(slice(var.domains, 0, 1))
-  alternative_domains   = length(var.domains) == 1 ? [] : slice(var.domains, 1, length(var.domains))
-  main_domain_sanitized = replace(local.main_domain, "*.", "")
-  custom_headers        = var.custom_headers != {} || length(var.s3_cors_rule) > 0 ? true : false
+  main_domain            = one(slice(var.domains, 0, 1))
+  alternative_domains    = length(var.domains) == 1 ? [] : slice(var.domains, 1, length(var.domains))
+  main_domain_sanitized  = replace(local.main_domain, "*.", "")
+  custom_headers_present = var.custom_headers != null && var.custom_headers != {}
+  custom_headers         = local.custom_headers_present || length(var.s3_cors_rule) > 0 ? true : false
   tags = merge({
     Name : local.main_domain_sanitized
   }, var.tags)
@@ -448,7 +449,7 @@ resource "aws_cloudfront_response_headers_policy" "this" {
 
   custom_headers_config {
     dynamic "items" {
-      for_each = var.custom_headers != {} && var.custom_headers.headers != null ? var.custom_headers.headers : {}
+      for_each = var.custom_headers.headers != null ? var.custom_headers.headers : {}
       content {
         header   = items.key
         value    = items.value.value
