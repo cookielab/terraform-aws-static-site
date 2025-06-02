e0d262d30412813ae237f6a9d0b11d74d188a9b3
Cookie Monster <robot@cookielab.io>
2025-06-02 08:09:28 +0200

    chore(deps): update terraform terraform-aws-modules/s3-bucket/aws to v4.10.0 (#51)

--- main.tf
+++ main.tf
@@ -190,7 +190,7 @@ data "aws_iam_policy_document" "kms_key_policy" {
 
 module "s3_bucket" {
   source  = "terraform-aws-modules/s3-bucket/aws"
-  version = "4.9.0"
+  version = "4.10.0"
 
   bucket = var.s3_bucket_name
 
